79c5b7329e543ad553bd66e9cf90e7df8d550c96
Nolan Posey <nposey1@ewu.edu>
2023-06-08 17:03:51 -0700

    Added simple functions to input or output text

--- note-app/src/views/QuillView.vue
+++ note-app/src/views/QuillView.vue
@@ -1,8 +1,25 @@
 <template>
-  <QuillEditor theme="snow" toolbar="full" />
+  <div>
+    <QuillEditor v-model:content="text" theme="snow" toolbar="full" />
+  </div>
+  <v-btn @click="getNote()">Save</v-btn>
+  <v-btn @click="setNote()">Edit</v-btn>
 </template>
 
 <script setup lang="ts">
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css'
+import { ref } from 'vue';
+
+let text = ref<string>('')
+let savedText = ''
+
+function getNote() {
+  console.log(text.value)
+  savedText = text.value
+}
+
+function setNote() {
+  text.value = savedText
+}
 </script>
