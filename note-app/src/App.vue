f690d2258799fd5d40e77b56efa1f270e486cba1
Nolan Posey <nposey1@ewu.edu>
2023-06-12 18:58:47 -0700

    Added Sign in service and dialog

--- note-app/src/App.vue
+++ note-app/src/App.vue
@@ -7,11 +7,22 @@
           Note App
         </RouterLink>
       </v-toolbar-title>
+
       <v-spacer />
+
+      <v-btn>
+        <span v-if="!signInService.isSignedIn" @click="signIn">Not signed in</span>
+        <span v-else>{{ signInService.token.userName }}</span>
+      </v-btn>
+
+      <v-btn v-if="signInService.isSignedIn" @click="signInService.signOut()">Sign Out</v-btn>
+
       <ToggleTheme />
+
     </v-app-bar>
 
     <v-main>
+      <SignInDialog v-model="showSignInDialog"></SignInDialog>
       <RouterView />
     </v-main>
   </v-app>
@@ -19,6 +30,17 @@
 
 <script setup lang="ts">
 import ToggleTheme from '@/components/Header/ToggleTheme.vue'
+import type { SignInService } from './scripts/signInService'
+import { ref, inject } from 'vue'
+import { Services } from './scripts/services'
+import SignInDialog from './components/SignInDialog.vue'
+
+const signInService = inject(Services.SignInService) as SignInService
+const showSignInDialog = ref(false);
+
+function signIn() {
+  showSignInDialog.value = true;
+}
 </script>
 
 <style scoped>
